b6be88225be9db849338b787b39466ec8b7b4582
Pisces000221 <1786762946@qq.com>
2015-05-23 19:17:43 +0800

    导入Cocos2d-JS库+创建空页面

---
+++ src/blast.js
@@ -0,0 +1,27 @@
+var blast = {};
+
+var EmptyScene = cc.Scene.extend({
+  onEnter: function () {
+    'use strict';
+    this._super();
+    cc.director.runScene(cc.TransitionFade.create(0.5, new cc.Scene(), cc.color(255, 0, 0)));
+  }
+});
+
+// I'll make it from here!
+window.onload = function() {
+  'use strict';
+  cc.game.onStart = function() {
+    cc.director.setDisplayStats(true);
+    cc.view.adjustViewPort(true);
+    if (cc.sys.isMobile) {
+      cc.view.setDesignResolutionSize(320, 480, cc.ResolutionPolicy.FIXED_WIDTH);
+    } else {
+      cc.view.setDesignResolutionSize(320, 480, cc.ResolutionPolicy.SHOW_ALL);
+    }
+    cc.view.resizeWithBrowserSize(true);
+    // resources were not large, so we just start running scenes directly
+    cc.director.runScene(new EmptyScene());
+  };
+  cc.game.run('game_canvas');
+};
