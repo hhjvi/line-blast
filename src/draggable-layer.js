52d89bf5d910a56358d53aa70ce5d54a78372338
Pisces000221 <1786762946@qq.com>
2015-05-23 22:43:27 +0800

    增加可以选中的列表项

--- src/draggable-layer.js
+++ src/draggable-layer.js
@@ -1,8 +1,14 @@
 // The draggable scroll view.
 // CCUI's scroll view is *really* plump...
+// TODO: Only supports children whose anchor points are exactly (0, 0)
+
+blast.inRegion = function (p, info) {
+  return p >= info.posY && p <= info.posY + info.height;
+};
 
 blast.Draggable = cc.Layer.extend({
-  _heightY: 0, _startPosY: 0, _dragStartY: 0,
+  _heightY: 0, _startPosY: 0, _dragStartPos: null,
+  _selectable: [], _selectedIdx: -1,
   ctor: function () {
     this._super();
     var _parent = this;
@@ -11,14 +17,35 @@ blast.Draggable = cc.Layer.extend({
       onTouchBegan: function (touch, event) {
         _parent._heightY = _parent.getContentSize().height - blast.vsize.height;
         _parent._startPosY = _parent.getPosition().y;
-        _parent._dragStartY = touch.getLocation().y;
+        _parent._dragStartPos = touch.getLocation();
+        var nodeSpacePosY = _parent.convertTouchToNodeSpace(touch).y;
+        for (var i in _parent._selectable) {
+          if (blast.inRegion(nodeSpacePosY, _parent._selectable[i])) {
+            _parent._selectedIdx = i;
+            _parent._selectable[i].node.activateSelect();
+            break;
+          }
+        }
         return true;
       },
       onTouchMoved: function (touch, event) {
-        var newPosY = _parent._startPosY + touch.getLocation().y - _parent._dragStartY;
+        if (_parent._selectedIdx !== -1
+            && cc.pDistanceSQ(_parent._dragStartPos, touch.getLocation()) >= 10) {
+          _parent._selectable[_parent._selectedIdx].node.deactivateSelect();
+          _parent._selectedIdx = -1;
+        }
+        var newPosY = _parent._startPosY + touch.getLocation().y - _parent._dragStartPos.y;
         if (newPosY > 0) newPosY = 0;
         else if (newPosY < -_parent._heightY) newPosY = -_parent._heightY;
         _parent.setPositionY(newPosY);
+      },
+      onTouchEnded: function (touch, event) {
+        // Often _selectedIdx is a string but -1 is set explicitly by our code, so === can work.
+        if (_parent._selectedIdx !== -1) {
+          _parent._selectable[_parent._selectedIdx].node.deactivateSelect();
+          _parent._selectable[_parent._selectedIdx].node.activateClick();
+          _parent._selectedIdx = -1;
+        }
       }
     }, this);
   },
@@ -27,5 +54,38 @@ blast.Draggable = cc.Layer.extend({
       this.setContentSize(this.getContentSize().width, blast.vsize.height);
     this._heightY = this.getContentSize().height - blast.vsize.height;
     this.setPositionY(-this._heightY);
+  },
+  addSelectableChild: function (child) {
+    this._selectable.push({
+      node: child, posY: child.getPosition().y, height: child.getContentSize().height
+    });
+    this.addChild(child);
+  }
+});
+
+var DLI_TitleFontSize = 36;
+var DLI_SubtitleFontSize = 26;
+var DLI_Height = 100;
+blast.DraggableListItem = cc.LayerColor.extend({
+  ctor: function (title, subtitle) {
+    this._super(cc.color(0, 0, 0));
+    this.setContentSize(cc.size(blast.vsize.width, DLI_Height));
+    var titleLabel = new cc.LabelTTF(title, '', DLI_TitleFontSize);
+    titleLabel.setAnchorPoint(cc.p(0, 0));
+    titleLabel.setPosition(cc.p(6, DLI_SubtitleFontSize + 12));
+    this.addChild(titleLabel);
+    var subtitleLabel = new cc.LabelTTF(subtitle, '', DLI_SubtitleFontSize);
+    subtitleLabel.setAnchorPoint(cc.p(0, 0));
+    subtitleLabel.setPosition(cc.p(6, 6));
+    this.addChild(subtitleLabel);
+  },
+  activateSelect: function () {
+    this.runAction(cc.tintTo(0.2, 0, 0, 144));
+  },
+  deactivateSelect: function () {
+    this.stopAllActions();
+    this.runAction(cc.tintTo(0.2, 0, 0, 0));
+  },
+  activateClick: function () {
   }
 });
--- src/level-list-scene.js
+++ src/level-list-scene.js
@@ -4,13 +4,14 @@ blast.LevelListScene = cc.Scene.extend({
     var lister = new blast.Draggable();
     lister.setContentSize(cc.size(blast.vsize.width, 1000));
     lister.setAnchorPoint(cc.p(0.5, 0));
+    lister.setPosition(cc.p(blast.vsize.width * 0.5, 0));
     lister.initPosition();
     this.addChild(lister);
-    for (var i = 0; i <= 10; ++i) {
-      var s = new cc.Sprite('res/back.png');
-      s.setNormalizedPosition(cc.p(0.5 + i * 0.05, i * 0.1));
-      s.setFlippedX(true);
-      lister.addChild(s);
+    for (var i = 0; i < 10; ++i) {
+      var s = new blast.DraggableListItem('Item ' + i, 'Lorem ipsum dolor sit amet');
+      s.setAnchorPoint(cc.p(0, 0));
+      s.setPosition(cc.p(-blast.vsize.width / 2, i * 100));
+      lister.addSelectableChild(s);
     }
   }
 });
